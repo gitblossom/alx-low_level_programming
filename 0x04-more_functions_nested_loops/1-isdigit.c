b38946bf38ed2f695e5de2adedc278dd20a2c1bf
gitblossom <dija.dahbi@gmail.com>
2023-09-14 14:18:59 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* int _isdigit(int c) checks for a digit
+* @c : char to check 
+*
+* return 1 or 0
+*/
+int _isdigit(int c)
+if (c >= '0' && c <= '9')
+return (1);
+else 
+return (0);
+}
