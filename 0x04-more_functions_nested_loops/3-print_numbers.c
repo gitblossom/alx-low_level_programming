e4f7dd6aa2d65fd5107ef3e94dd8e890e1fe2cd3
gitblossom <dija.dahbi@gmail.com>
2023-09-14 14:44:54 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers-print numbers from 0 to 9
+* 
+* return: void
+*/
+void print_numbers(void)
+{
+char i;
+while (i = '0'; i <= 9; i++)
+_putchar(i);
+_putchar('\n');
+}
