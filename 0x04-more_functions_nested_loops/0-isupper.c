c19e6348dccdb2783a4e9c7b7b8ff27240bb1006
gitblossom <dija.dahbi@gmail.com>
2023-09-14 13:53:56 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** int _isupper(int c) checks for uppercase characters
+*
+* return 1 if is uppercase and 0 othewise
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else 
+return (0);
+}
